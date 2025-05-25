cecc2db25fa3efb5ef91c0bc71ee2113d5c52a31
jan <>
2025-05-25 21:04:07 +0000

    duplication handle was missing
    FossilOrigin-Name: eb90bc65067b94ecc2693ee3ebb8f24a19d2a09fe8d5a1661df2f56cb2b43243

---
+++ src/handles/duplicationHandle.ts
@@ -0,0 +1,102 @@
+import { Handle } from "./handle.js";
+import { LocalDragEvent, LocalMouseEvent } from "../events.js";
+import { DrawingView } from "../drawingView.js";
+import { Figure } from "../figures/figure.js";
+import { Rect } from "../data/rect.js";
+import { Point } from "../data/point.js";
+import { DuplicateFigureCommand } from "../commands/duplicateFigureCommand.js";
+
+/**
+ * Adds a handle that allows to duplicate a figure upon click or by dragging. 
+ * Similar to the usual pressing of alt+drag, but without a key.
+ */
+class DuplicationHandle extends Handle{
+    #size = 16;
+   
+    constructor(figure:Figure,drawingView:DrawingView){
+        super(figure,drawingView);
+    }
+    draw(ctx){
+        const {x,y,width,height} = this.getScreenRect();
+        ctx.fillStyle = "#c3e7ee";
+        ctx.fillRect(x,y,width,height);
+        ctx.strokeStyle = "#9bbac0";
+        ctx.strokeRect(x,y,width,height);
+
+        //draw a little duplication icon:
+        ctx.strokeStyle = "#294145";
+        ctx.strokeRect(x+3, y+3,8,7);
+        ctx.strokeRect(x+5, y+5,8,7);
+    }
+    getScreenRect():Rect{
+        const drawingView = this.getDrawingView();
+        const figure = this.getFigure();
+        const figureRect = figure.getRect();
+        const {bottomRight} = figureRect.getCorners();
+        const drawAnchorScreen = drawingView.documentToScreenPosition(bottomRight);
+        const screenRect = new Rect({
+            x:      drawAnchorScreen.x + (this.#size *2),
+            y:      drawAnchorScreen.y,
+            width:  this.#size,
+            height: this.#size
+        });
+        return screenRect;
+    }
+    #isDocumentPointInHandle(documentPoint:Point){
+        const screenRect = this.getScreenRect();
+        const drawingView = this.getDrawingView();
+        const screenPoint = drawingView.documentToScreenPosition(documentPoint);
+        const isPointInHandle = screenRect.isEnclosingPoint(screenPoint);
+        return isPointInHandle;
+    }
+    #createChangedRect(dragMovement:Point){
+        //get drag movement, return new Rectangle
+        const figure = this.getFigure();
+        const rect = figure.getRect();
+        const newRect = rect.movedCopy(dragMovement);
+        return newRect;
+    }
+    onMouseUp(mouseEvent:LocalMouseEvent){
+        // if mouseup is still in the handle, copy the figure with a minor offset
+        // implement after dragging works
+    }
+    onDragstart(dragEvent:LocalDragEvent):void{
+        // start preview
+        const drawingView = this.getDrawingView();
+        drawingView.startPreviewOf(this.getFigure(),false); 
+    }
+    onDrag(dragEvent:LocalDragEvent){
+        const drawingView = this.getDrawingView();
+        const dragMovement = dragEvent.getDocumentDragMovement();
+        const previewFigure = drawingView.getPreviewedFigure();
+        const newPreviewFigureRect = this.#createChangedRect(dragMovement);
+        previewFigure.setRect(newPreviewFigureRect);
+    }
+    onDragend(dragEvent:LocalDragEvent){
+        //check if copy can be placed
+        const drawingView = this.getDrawingView();
+        const dragMovement = dragEvent.getDocumentDragMovement();
+        const resizedFigureRect = this.#createChangedRect(dragMovement);
+        const isInBounds = drawingView.drawing.isEnclosingRect(resizedFigureRect);
+        if(!isInBounds){
+            console.log("Changed Figure would be out of bounds, aborting command");
+            return;
+        }
+        const duplicateFigureCommand = new DuplicateFigureCommand({figure:this.getFigure(), newRect:resizedFigureRect},drawingView)
+        drawingView.do(duplicateFigureCommand);
+    }
+    dragExit(){
+        const drawingView = this.getDrawingView();
+        drawingView.endPreview();
+    }
+    getInteractions(){
+        return { 
+            cursor: "grab",
+            helpText: "duplicates figure by dragging",
+            draggable: true, 
+            clickable: false 
+        };
+    }
+}
+
+export {DuplicationHandle}
