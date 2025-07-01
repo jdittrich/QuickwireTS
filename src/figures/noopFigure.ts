b12b8f39f73627493aa5f776e81e42c800ab6d6d
jan <>
2025-07-01 20:32:41 +0000

    fixing visible noOp figure (issue #13 on github)
    FossilOrigin-Name: 3458ded9f0b4b78a3d46247b7004b64531ba08395b0393e9b012ed845fb20702

--- src/figures/noopFigure.ts
+++ src/figures/noopFigure.ts
@@ -1,5 +1,7 @@
 import { Figure } from "./figure.js";
 import { Rect } from "../data/rect.js";
+import { DrawingView } from "../drawingView.js";
+import { Handle } from "../handles/handle.js";
 
 class NoOpFigure extends Figure{
     constructor(){
@@ -7,8 +9,8 @@ class NoOpFigure extends Figure{
             rect: new Rect({
                 x:0,
                 y:0,    
-                width:1,
-                height:1
+                width:0,
+                height:0
             }),
             containedFigures:[]
         });
@@ -17,6 +19,11 @@ class NoOpFigure extends Figure{
     //overwrite methods that do anything in base Figure
     drawFigure(ctx:CanvasRenderingContext2D):void{}
     drawHighlight(ctx: CanvasRenderingContext2D): void {}
+    getHandles(drawingView: DrawingView): Handle[] {
+        return []
+    }
+    
+
 }
 
 export {NoOpFigure}
